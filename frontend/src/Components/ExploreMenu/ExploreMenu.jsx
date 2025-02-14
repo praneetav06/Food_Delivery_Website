400705f9013d9db1c9ce919eddfc64ecaeb7959e
Praneeta Verma <praneetav06@gmail.com>
2025-02-14 15:11:18 +0530

    added FoodItem and FoodDisplay component using StoreContext

--- frontend/src/Components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/Components/ExploreMenu/ExploreMenu.jsx
@@ -9,8 +9,8 @@ const ExploreMenu = ({category, setCategory}) => {// mount the component in home
             <div className="explore-menu-list">
                 {menu_list.map((item, index)=>{
                     return (
-                        <div key={index} className="explore-menu-list-item">
-                            <img src={item.menu_image} alt="" />
+                        <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
+                            <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
                             <p>{item.menu_name}</p>
                         </div>
                     )
