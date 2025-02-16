bf4004d29a735a96a66f500e276f6272c8d01106
Praneeta Verma <praneetav06@gmail.com>
2025-02-16 16:25:59 +0530

    created footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar';
-import { Route, Routes } from 'react-router-dom';
-import Home from './Pages/Home/Home';
-import Cart from './Pages/Cart/Cart';
-import PlaceOrder from './Pages/PlaceOrder/PlaceOrder';
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Cart from "./Pages/Cart/Cart";
+import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
+import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar/>
-      <Routes> 
-        <Route path='/' element={<Home/>} />
-        <Route path='/Cart' element={<Cart/>} />
-        <Route path='/PlaceOrder' element={<PlaceOrder/>} />
-      </Routes>
-    </div>
-  )
-}
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Cart" element={<Cart />} />
+          <Route path="/PlaceOrder" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
 export default App;
---
+++ frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./footer.css";
+import { assets } from "../../assets/assets";
+const Footer = () => {
+  //mount footer in app component
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Satisfy your cravings with Tomato! Fresh, delicious meals delivered
+            straight to your door. Whether you're in the mood for local
+            favorites or international cuisine, we've got you covered.
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+0-123-456-7890</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright @Tomato.com - All Rights Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
