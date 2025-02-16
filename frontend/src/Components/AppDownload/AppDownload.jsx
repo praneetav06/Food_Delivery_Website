ea8d3b3a72171422f249973e7adbfbbf3e27e485
Praneeta Verma <praneetav06@gmail.com>
2025-02-16 17:26:35 +0530

    created AppDownload component

---
+++ frontend/src/Components/AppDownload/AppDownload.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./AppDownload.css";
+import { assets } from "../../assets/assets";
+
+const AppDownload = () => {
+  //mount the component in Home.jsx
+  return (
+    <div className="app-download" id="app-download">
+      <p>
+        For better experience Download <br /> Tomato App
+      </p>
+      <div className="app-download-platforms">
+        <img src={assets.play_store} alt="" />
+        <img src={assets.app_store} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default AppDownload;
