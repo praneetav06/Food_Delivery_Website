d615ccf92d133a10f8bec74b3d3e9bcb0e464220
Praneeta Verma <praneetav06@gmail.com>
2025-02-20 16:15:57 +0530

    finished structure for navbar and sidebar and created components in pages

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Sidebar from './Components/Sidebar/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import Add from './Pages/Add/Add';
+import List from './Pages/List/List';
+import Orders from './Pages/Orders/Orders';
 
 const App = () => {
   return (
@@ -9,6 +13,11 @@ const App = () => {
       <hr />
       <div className="app-content">
         <Sidebar/>
+        <Routes>
+          <Route path='/Add' element={<Add/>} />
+          <Route path='/List' element={<List/>} />
+          <Route path='/Orders' element={<Orders/>} />
+        </Routes>
       </div>
     </div>
   )
