25c26551a84b86682322adcde62ab072e8aff957
Praneeta Verma <praneetav06@gmail.com>
2025-02-27 15:13:16 +0530

    created middleware and addToCart function

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,7 +1,22 @@
 import userModel from "../models/userModel.js";
 
 //add items to your cart
-const addToCart = async (req, res) => {};
+const addToCart = async (req, res) => {
+  try {
+    let userData = await userModel.findOne({ _id: req.body.userId });
+    let cartData = await userData.cartData;
+    if (!cartData[req.body.itemId]) {
+      cartData[req.body.itemId] = 1;
+    } else {
+      cartData[req.body.itemId] += 1;
+    }
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    res.json({ success: true, message: "Added to Cart" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
 //remove items from user cart
 const removeFromCart = async (req, res) => {};
