b6a52365c8c5308eb19d58e6df9b62bdc1842146
Praneeta Verma <praneetav06@gmail.com>
2025-03-04 15:42:31 +0530

    fixed env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,6 @@
 import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
-    .connect(
-      "mongodb+srv://praneetav06:eLz1jTHs3t8uVUXm@fooddeliverywebsite.1j0qr.mongodb.net/Food_Delivery_Website"
-    )
+    .connect(process.env.ATLAS_PWD)
     .then(() => console.log("connected to DB"));
 };
