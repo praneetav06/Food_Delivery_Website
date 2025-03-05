fecf9e988c96a3d162d0b94f49a881055197d96b
Praneeta Verma <praneetav06@gmail.com>
2025-03-05 15:39:43 +0530

    fixed import statement

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from "react";
 import "./Navbar.css";
-import { assets } from "../../assets/assets";
+import { assets } from "../../assets/assets.js";
 import { Link, useNavigate } from "react-router-dom";
 import { StoreContext } from "../../Context/StoreContext";
 
