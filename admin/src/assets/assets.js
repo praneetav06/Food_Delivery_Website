ebab630fdda6a747ba234dde971fd4806ae9cd31
Praneeta Verma <praneetav06@gmail.com>
2025-03-05 15:25:32 +0530

    added deployed backend url

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets = {
   parcel_icon,
 };
 
-export const url = "https://food-delivery-website-backend-ac0z.onrender.com";
+export const url = "https://food-delivery-website-backend-43wv.onrender.com";
--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "https://food-delivery-website-backend-ac0z.onrender.com";
+  const url = "https://food-delivery-website-backend-43wv.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]); // fetch data from db
   const addToCart = async (itemId) => {
